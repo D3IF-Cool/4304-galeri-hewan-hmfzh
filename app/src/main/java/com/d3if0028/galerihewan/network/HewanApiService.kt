f4829c36f9fcd3d66450385f9ffdf174bd6a791a
Muhammad Ilham Hafizha <ilham.hafizh7@gmail.com>
2021-05-05 10:36:39 +0700

    Mengambil data dari internet

---
+++ app/src/main/java/com/d3if0028/galerihewan/network/HewanApiService.kt
@@ -0,0 +1,23 @@
+package com.d3if0028.galerihewan.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+    private const val BASE_URL = "https://dif.indraazimi.com/"
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    interface HewanApiService {
+        @GET("listhewan.json")
+        suspend fun getHewan(): String
+    }
+
+    object HewanApi {
+        val service: HewanApiService by lazy {
+            retrofit.create(HewanApiService::class.java)
+        }
+    }
--- app/src/main/java/com/d3if0028/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/com/d3if0028/galerihewan/ui/main/MainViewModel.kt
@@ -1,16 +1,31 @@
 package com.d3if0028.galerihewan.ui.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.d3if0028.galerihewan.R
 import com.d3if0028.galerihewan.model.Hewan
+import com.d3if0028.galerihewan.network.HewanApi
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel(){
     private val data = MutableLiveData<List<Hewan>>()
 
     init {
         data.value = initData()
+        retrieveData()
+    }
+    private fun retrieveData() {
+        viewModelScope.launch {
+            try {
+                val result = HewanApi.service.getHewan()
+                 Log.d("MainViewModel", "Success: $result")
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+            }
+        }
     }
 
     private fun initData(): List<Hewan> {
